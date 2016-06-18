cd8e12a5790c59fc22e764dfdb68559768fc918e
mikadonald <renvy031218@gmail.com>
2016-06-17 17:06:51 -0700

    basics

--- basics.py
+++ basics.py
@@ -1,6 +1,7 @@
 import sys
 import random as rd
 from scipy.stats import norm
+import numpy as np
 
 def random_walk(steps):
     	step = 0
@@ -21,7 +22,7 @@ def random_walk(steps):
     	return routine
 
 
-def brownian_motion(steps, dt, delta):
+def brownian_motion(steps, dt):
         step = 0
         routine = []
         location = 0
@@ -33,12 +34,31 @@ def brownian_motion(steps, dt, delta):
             sys.exit("Error message! Steps should be a positive integer!")
 
         while (currentstep < steps):
-            location += norm.rvs(scale=delta**2*dt)
+            location += norm.rvs(scale=delta*np.sqrt(dt))
             routine.append(location)
             currentstep += 1
 
         return routine
 
+def geometric_brownian_motion(steps, dt, mu, delta, starting_point):
+        step = 0
+        routine = []
+        location = 0
+        currentstep = 0
+        t = dt 
+
+        if (steps >0):
+            routine.append(0)
+        else:
+            sys.exit("Error message! Steps should be a positive integer!")
+
+        while (currentstep < steps):
+            location = starting_point * np.exp((mu - 0.5 * delta ** 2) * t + norm.rvs(scale=delta*np.sqrt(t)))
+            routine.append(location)
+            currentstep += 1
+            t += dt
+
+        return routine
 
 
 
